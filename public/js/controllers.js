86354ab27fd786bd0abef67935e5c2c22cb829f0
Ted Wu <tedwu2016@u.northwestern.edu>
2015-04-26 16:32:21 -0500

    fix bugs

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -12,24 +12,34 @@ instagramApp.controller('indexController', ['$scope', function($scope) {
 instagramApp.controller('instagramController', ['$scope', 'Instagram',
 	function($scope, Instagram){
 		
+		$scope.layout = 'grid';
+
+
+		$scope.setLayout = function(layout){
+			$scope.layout = layout;
+		}
+
+		$scope.isLayout = function(layout){
+			return $scope.layout == layout;
+		}
 
 		$scope.example1 = {
-			hash: 'angular'
+			hash: 'pinerowedding2015'
 		};
 
 		$scope.example2 = {
-			hash: 'instago'
+			hash: 'BrideToBe'
 		};
 
 		$scope.example3 = {
-			hash: 'examples'
+			hash: 'FlowerGirl'
 		};
 
 		var instagramSuccess = function(scope, res) {
-			if (res.meta.code !== 200) {
-				scope.error = res.meta.error_type + ' | ' + res.meta.error_message;
-				return;
-			}
+			// if (res.meta.code !== 200) {
+			// 	scope.error = res.meta.error_type + ' | ' + res.meta.error_message;
+			// 	return;
+			// }
 			if (res.data.length > 0) {
 				scope.items = res.data;
 			} else {
@@ -37,15 +47,15 @@ instagramApp.controller('instagramController', ['$scope', 'Instagram',
 			}
 		};
 
-		Instagram.get(100, $scope.example1.hash).success(function(response) {
+		Instagram.get(10, $scope.example1.hash).success(function(response) {
 			instagramSuccess($scope.example1, response);
 		});
 
-		Instagram.get(9, $scope.example2.hash).success(function(response) {
+		Instagram.get(10, $scope.example2.hash).success(function(response) {
 			instagramSuccess($scope.example2, response);
 		});
 
-		Instagram.get(9, $scope.example3.hash).success(function(response) {
+		Instagram.get(10, $scope.example3.hash).success(function(response) {
 			instagramSuccess($scope.example3, response);
 		});
 
