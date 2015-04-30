732ccebdec6ec07b22e7bba0ad8e39b8d2ec5d35
Ted Wu <tedwu2016@u.northwestern.edu>
2015-04-29 20:26:47 -0500

    timeline js

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -11,24 +11,71 @@ instagramApp.controller('indexController', ['$scope',
 
 
 
-instagramApp.controller('instagramController', ['$scope', 'Instagram',
-	function($scope, Instagram){
+instagramApp.controller('instagramController', ['$scope', 'Instagram', '$http',
+	function($scope, Instagram, $http){
 
 		$scope.example1 = {
 			hash: 'marcheet'
 		};
 
-		$scope.example2 = {
-			hash: 'hanksparty'
-		};
+		// $scope.example2 = {
+		// 	hash: 'hanksparty'
+		// };
 
-		$scope.example3 = {
-			hash: 'pagingthemarcels'
-		};
+		// $scope.example3 = {
+		// 	hash: 'pagingthemarcels'
+		// };
 
 		var instagramSuccess = function(scope, res) {
 			if (res.data.length > 0) {
 				scope.items = res.data;
+				var testObject = {
+					    "timeline":
+					    {
+					        "headline":"WEDDING PHOTO",
+					        "type":"default",
+					        "text":"<p>FIRST PHOTO OF INSTAGRAM</p>",
+					        "asset": {
+					            "media": res.data[0].images.standard_resolution.url,
+					            "credit":"Credit Name Goes Here",
+					            "caption":"Caption text goes here"
+					        },
+					        "date": [
+					            {
+					                "startDate":"2011,12,10",
+					                "endDate":"2011,12,11",
+					                "headline":"Headline Goes Here",
+					                "text":"<p>Body text goes here, some HTML is OK</p>",
+					                "tag":"This is Optional",
+					                "classname":"optionaluniqueclassnamecanbeaddedhere",
+					                "asset": {
+					                    "media":"http://twitter.com/ArjunaSoriano/status/164181156147900416",
+					                    "thumbnail":"optional-32x32px.jpg",
+					                    "credit":"Credit Name Goes Here",
+					                    "caption":"Caption text goes here"
+					                }
+					            }
+					        ],
+					        "era": [
+					            {
+					                "startDate":"2011,12,10",
+					                "endDate":"2011,12,11",
+					                "headline":"Headline Goes Here",
+					                "tag":"This is Optional"
+					            }
+
+					        ]
+
+					    }
+					}
+				createStoryJS({
+                    type:       'timeline',
+                    width:      '800',
+                    height:     '600',
+                    source:     testObject,
+                    embed_id:   'my-timeline'
+                });
+
 			} else {
 				scope.error = "This hashtag has returned no results";
 			}
@@ -45,6 +92,15 @@ instagramApp.controller('instagramController', ['$scope', 'Instagram',
 		Instagram.get(20, $scope.example3.hash).success(function(response) {
 			instagramSuccess($scope.example3, response);
 		});
+
+		$http.get('tests.json').
+		    success(function(data, status, headers, config) {
+		      $scope.tests = data;
+		      
+		    }).
+		    error(function(data, status, headers, config) {
+		      console.log("error");
+		    });
 
 	}
 
