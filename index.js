418bb1e6256a3a36999ebda57b4d99227f4c1d44
patrickfweston <patrickfweston@gmail.com>
2015-05-03 23:39:01 -0500

    Converted Instagram JSON to Timeline.js friendly JSON

--- index.js
+++ index.js
@@ -1,16 +1,70 @@
 var express = require('express');
+var request = require('request');
+var moment = require('moment');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
 app.set('port', (process.env.PORT || 8000));
 
-app.get('/test', function(request, response) {
-  response.json({"location": "test"});
+app.get('/feed', function(req, res) {
+  //have the base instagram URL 
+  var instagramURL = "https://api.instagram.com/v1";
+  // get your own client id http://instagram.com/developer/
+  var clientId = '6a4536f6c1334d1cb0e37519930b084c';
+
+  var instaReq = '/tags/' + req.query.hashtag + '/media/recent?';
+  var fullUrl = instagramURL + instaReq;
+
+  var config = {
+    client_id: clientId,
+    count: req.query.count,
+  };
+
+  var data = "";
+  request({url:fullUrl, qs: config})
+    .on('data', function(d) {
+      data += d;
+    })
+    .on('end', function() {
+      data = JSON.parse(data);
+      res.json(instaToTimeline(data, req.query.hashtag));
+    });
 });
 
+function instaToTimeline(data, htag) {
+  var instaObj = {
+    "timeline": {
+      "headline": "#" + htag + " wedding",
+      "type":"default",
+      "text": "Some body text here",
+      "date": [],
+    }
+  }
+
+  for (i = 0; i < (data.data).length; i++) {
+    row = data.data[i];
+
+    tempDate = moment(new Date(row.created_time * 1000));
+    dateString = tempDate.format("YYYY,MM,DD,HH,mm,ss")
+
+
+    instaObj.timeline.date[i] = {
+      "startDate": dateString,
+      "headline": "#" + htag + " wedding",
+      "text": row.caption.text,
+      "asset": {
+       "media": row.images.standard_resolution.url,
+        "credit": "@" + row.user.username
+      },
+    }
+  }
+
+  return JSON.parse(JSON.stringify(instaObj));
+}
+
 app.get('*', function (req, res) {
-    res.sendFile('/public/index.html');
+    res.sendFile('index.html');
 });
 
 app.listen(app.get('port'), function() {
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -1,102 +1,68 @@
-var instagramApp = angular.module('instagramApp', []);
+var instagramApp = angular.module('instagramApp', [])
+	.factory('feedData', function() {
+		var hashtag = 'default';
 
-instagramApp.controller('indexController', ['$scope', 
-	function($scope){
+		return {
+			setHashtag: function (x) {
+				console.log(x);
+				hashtag = x;
+			},
+			getHashtag: function () {
+				return hashtag;
+			}
+		};
+	});
+
+instagramApp.controller('indexController', ['$scope', 'feedData', '$window',
+	function($scope, feedData, $window){
 
 	$scope.generate = function(){
-  		alert("generating timeline");
-  	};
+		feedData.setHashtag('marcheet');
+		console.log(feedData.getHashtag());
+		$window.location.href = '/results.html';
+	};
 
 }]);
 
 
-
-instagramApp.controller('instagramController', ['$scope', 'Instagram', '$http',
-	function($scope, Instagram, $http){
-
-		$scope.example1 = {
-			hash: 'marcheet'
-		};
-
-		$scope.example2 = {
-			hash: 'hanksparty'
-		};
-
-		$scope.example3 = {
-			hash: 'pagingthemarcels'
-		};
-
-		var instagramSuccess = function(scope, res) {
-			if (res.data.length > 0) {
-				scope.items = res.data;
-				console.log(res.data[0])
-
-				instaItem = res.data[0];
-				var testObject = {
-					    "timeline":
-					    {
-					        "headline":"WEDDING PHOTO",
-					        "type":"default",
-					        "text": instaItem.caption.text,
-					        "asset": {
-					            "media": instaItem.images.standard_resolution.url,
-					            "credit": "@" + instaItem.user.username
-					        },
-					        "date": [
-					            {
-					                "startDate": Date(instaItem.created_time * 1000),
-					                "headline":"Headline Goes Here",
-					                "text":"<p>Body text goes here, some HTML is OK</p>",
-					                "tag":"This is Optional",
-					                "classname":"optionaluniqueclassnamecanbeaddedhere",
-					                "asset": {
-					                    "media":"http://twitter.com/ArjunaSoriano/status/164181156147900416",
-					                    "thumbnail":"optional-32x32px.jpg",
-					                    "credit":"Credit Name Goes Here",
-					                    "caption":"Caption text goes here"
-					                }
-					            }
-					        ],
-					        
-
-					    }
-					}
-				createStoryJS({
-                    type:       'timeline',
-                    width:      '800',
-                    height:     '600',
-                    source:     testObject,
-                    embed_id:   'my-timeline'
-                });
-
-			} else {
-				scope.error = "This hashtag has returned no results";
-			}
-		};
-
-		Instagram.get(20, $scope.example1.hash).success(function(response) {
-			instagramSuccess($scope.example1, response);
+instagramApp.controller('instagramController', ['$scope', 'Instagram', '$http', 'feedData',
+	function($scope, Instagram, $http, feedData){
+		feedUrl = '/feed/?hashtag=' + feedData.getHashtag() +'&count=20';
+		console.log(feedUrl);
+		createStoryJS({
+		  type:       'timeline',
+		  width:      '800',
+		  height:     '600',
+		  source:     feedUrl,
+		  embed_id:   'my-timeline'
 		});
-
-		// Instagram.get(20, $scope.example2.hash).success(function(response) {
-		// 	instagramSuccess($scope.example2, response);
-		// });
-
-		// Instagram.get(20, $scope.example3.hash).success(function(response) {
-		// 	instagramSuccess($scope.example3, response);
-		// });
-
-		$http.get('tests.json').
-		    success(function(data, status, headers, config) {
-		      $scope.tests = data;
-		      
-		    }).
-		    error(function(data, status, headers, config) {
-		      console.log("error");
-		    });
-
 	}
 
+		// $scope.example1 = {
+		// 	hash: 'marcheet'
+		// };
 
+		// $scope.example2 = {
+		// 	hash: 'hanksparty'
+		// };
+
+		// $scope.example3 = {
+		// 	hash: 'pagingthemarcels'
+		// };
+
+		// var instagramSuccess = function(scope, res) {
+		// 	if (res.data.length > 0) {
+		// 		scope.items = res.data;
+		// 		console.log(res.data[0])
+
+		// 		instaItem = res.data[0];
+		// 	} else {
+		// 		scope.error = "This hashtag has returned no results";
+		// 	}
+		// };
+
+		// Instagram.get(20, $scope.example1.hash).success(function(response) {
+		// 	instagramSuccess($scope.example1, response);
+		// });
 ]);
 
