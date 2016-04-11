bbe9b3fa78e380e1e1c0414281380e6ab4296929
Ted Wu <tedwu2016@u.northwestern.edu>
2016-04-11 12:01:13 -0500

    push to heroku

--- public/js/factory.js
+++ public/js/factory.js
@@ -1,13 +1,11 @@
 'use strict';
 
-instagramApp.factory('Instagram', ['$http', function($http) {
-		
+instagramApp.factory('Instagram', ['$http', function($http) {		
 		//have the base instagram URL 
 		var instagramURL = "https://api.instagram.com/v1";
 		// get your own client id http://instagram.com/developer/
 		var clientId = '6a4536f6c1334d1cb0e37519930b084c';
-
-
+		
 		return {
 			'get': function(count, hashtag) {
 				var request = '/tags/' + hashtag + '/media/recent';
